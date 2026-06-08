f8bc4818562be756181f5cd8f9fca07dc91754ef
Elvean <hello@elvean.app>
2026-06-08 12:32:16 -0700

    Fix: remove iOS platform target (CLI is macOS-only, SPI build failure)

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ import PackageDescription
 let package = Package(
     name: "swift-edgar",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16)
+        .macOS(.v13)
     ],
     products: [
         .library(name: "Edgar", targets: ["Edgar"]),
